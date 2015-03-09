cb97e0ebee2bf7ebe929b7498abed0b23cd30238
crisb <crisb@localhost.localdomain>
2015-03-09 16:59:44 +0000

    Obsolete, merged back into main firefox package

--- download.sh
+++
@@ -1,6 +0,0 @@
-langlist="af ar as ast be bg bn-IN bn-BD br bs ca cs cy da de el en-GB en-ZA eo es-AR es-CL es-ES es-MX et eu fa fi fr fy-NL ga-IE gd gl gu-IN he hi-IN hr hu hy-AM id is it ja kk ko km kn lt lv mai mk ml mr nb-NO nl nn-NO or pa-IN pl pt-BR pt-PT ro ru si sk sl sq sr sv-SE ta te th tr uk vi zh-CN zh-TW"
-
-fversion=`grep ^Version: firefox-l10n.spec  | awk '{print $2}'`
-
-echo "Fetching $fversion files"
-for i in $langlist;do wget http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/$fversion/linux-i686/xpi/$i.xpi;done
